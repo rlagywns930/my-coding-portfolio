caf38fa7a1f925492d04b51b6bc29f6f4202b16b
rlagywns930 <kimhujon@gmail.com>
2025-07-22 22:40:45 +0900

    Update 03. 조건문.py

--- 02_python-coding/03. 조건문.py
+++ 02_python-coding/03. 조건문.py
@@ -1,1 +1,16 @@
+## 동전 던지기 게임
 
+# random 모듈을 불러와서 무작위 수를 생성
+import random
+
+print("동전 던지기 게임을 지금 시작합니다.")
+
+# 0 또는 1 중 무작위 값을 생성하여 coin 변수에 저장
+coin = random.randrange(2)
+
+# 동전의 결과에 따라 앞면 또는 뒷면 출력
+if coin == 0 :
+    print("앞면입니다.")
+else :
+    print("뒷면입니다.")
+print("게임이 종료되었습니다.")
